1912c995fba6d1cc1af97c78d10dc355c1febcf3
aakzsh <aakashferrari@gmail.com>
2021-09-20 20:24:10 +0530

    get frames from video

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+import cv2
+import extract as ext
 
 
 # port = int(os.environ.get('PORT', 5000))
@@ -11,6 +13,34 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/enhance')
+def enhance():
+    return render_template('enhance.html')
+
+@app.route('/analyse')
+def analyse():
+    return render_template('analyse.html')
+
+@app.route("/extract_frames/", methods=['POST'])
+def extract_frames():
+    print("started")
+    cap= cv2.VideoCapture('E:/test.mp4') # add file path here dynamically
+    cap.set(cv2.CAP_PROP_FPS, 60) # this isnt working rn idk why, taking 30fps only
+    i=0
+    while(cap.isOpened()):
+        ret, frame = cap.read()
+        if ret == False:
+            break
+        cv2.imwrite('frames/'+str(i)+'.png',frame)
+        i+=1
+    cap.release()
+    cv2.destroyAllWindows()
+    print(i)
+    return str(i)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
