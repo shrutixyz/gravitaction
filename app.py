0ea106653f6c77c5d4d128d99d810fba80c5cbd5
aakzsh <aakashferrari@gmail.com>
2021-09-21 16:20:27 +0530

    select video from any location

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import cv2
 import extract as ext
+import glob
 
 
 # port = int(os.environ.get('PORT', 5000))
-
+UPLOAD_FOLDER = 'uploadFile'
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 # app.config["DEBUG"] = True
 
 
@@ -21,10 +23,12 @@ def enhance():
 def analyse():
     return render_template('analyse.html')
 
-@app.route("/extract_frames/", methods=['POST'])
+@app.route("/extract_frames", methods=['POST'])
 def extract_frames():
+    video = request.files['file1']
+    video.save('video/video.mp4')
     print("started")
-    cap= cv2.VideoCapture('E:/test.mp4') # add file path here dynamically
+    cap= cv2.VideoCapture('video/video.mp4') # add file path here dynamically
     cap.set(cv2.CAP_PROP_FPS, 60) # this isnt working rn idk why, taking 30fps only
     i=0
     while(cap.isOpened()):
@@ -36,6 +40,22 @@ def extract_frames():
     cap.release()
     cv2.destroyAllWindows()
     print(i)
+    print("made frames")
+    # gotta add photo enhance function and put into array and join
+    # img_array = []
+    # for filename in glob.glob('frames/*.jpg'):
+    #     img = cv2.imread(filename)
+    #     height, width, layers = img.shape
+    #     size = (width,height)
+    #     img_array.append(img)
+ 
+    
+    # out = cv2.VideoWriter('final/final.mp4',cv2.VideoWriter_fourcc(*'DIVX'), 15, size)
+ 
+    # for i in range(len(img_array)):
+    #     out.write(img_array[i])
+    # out.release()
+    # print("ban gayi firse video") #can be find in final.mp4, should be given to users to download
     return str(i)
 
 
